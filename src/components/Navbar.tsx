2cab7e0dbc38ea72bd9e2376b8e5857dc2de12d4
adriana.rugeles <adriana.rugeles@aspsols.com>
2023-12-04 18:57:49 -0500

    add navbar component and folder for images

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,1 +1,30 @@
-const NavBar = () => {}
+import Link from "next/link";
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import { Icons } from "./ui/Icons";
+import NavItems from "./NavItems";
+
+const NavBar = () => {
+  return (
+    <div className="bg-white sticky z-50 top-0 inset-x-0 h-16 ">
+      <header className="relative bg-white">
+        <MaxWidthWrapper>
+          <div className="flex border-b border-gray-200 p-4">
+            {/* RODO: mobile nav */}
+
+            <div className="ml-4 flex lg:ml-0">
+              <Link href="/">
+                <Icons.logo className="h-10 w-10" />
+              </Link>
+            </div>
+
+            <div className="hidden z-50 lg:ml-8 lg:block lg:self-stretch">
+              <NavItems></NavItems>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </header>
+    </div>
+  );
+};
+
+export default NavBar;
